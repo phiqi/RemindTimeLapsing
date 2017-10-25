83f6a3aea34deea174ae7942fc7c37297ec3082f
IQEQSQ <940544014@qq.com>
2017-10-25 16:46:36 +0800

    完善

--- WPFClock/WPFClock/MainWindow.xaml.cs
+++ WPFClock/WPFClock/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Timers;
+using System.Runtime.InteropServices;
 
 namespace WPFClock
 {
@@ -8,8 +9,13 @@ namespace WPFClock
     {
         private Timer m_timer;
         private int m_nSecond;
+        private int m_nHalfHourSecond;
         //private Action m_action;  //会报“不允许跨线程访问”
         private delegate void UpdateTimer();    //通过委托来调用定时器方法，否则会报“不允许跨线程访问”
+
+        [DllImport("user32.dll")]
+        private static extern void LockWorkStation();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,8 +25,9 @@ namespace WPFClock
             m_timer.Interval = 1000;
             m_timer.Elapsed += HandleElapsed;
             m_timer.Start();
+            LockWorkStation();
             Height = SystemParameters.PrimaryScreenHeight;
-            Width = SystemParameters.PrimaryScreenWidth;
+            Width = SystemParameters.PrimaryScreenWidth * 3;
             //m_action = MyEventFunc;
         }
 
@@ -33,6 +40,7 @@ namespace WPFClock
         void MyEventFunc()
         {
             m_nSecond++;
+            m_nHalfHourSecond++;
             if (m_nSecond == 1)
             {
                 Hide();
@@ -41,6 +49,12 @@ namespace WPFClock
             {
                 m_nSecond = 0;
                 Show();
+            }
+
+            else if (m_nHalfHourSecond == 1800)
+            {
+                m_nHalfHourSecond = 0;
+                LockWorkStation();
             }
         }
     }
